83d0aa5ac93a01ce313a1e05b214367c802fd6ef
Ilmari Marttila <ilmari.marttila@tuni.fi>
2020-03-04 18:18:31 +0200

    Lisätty eka kuva lämpöpumpusta ja hiukan selostusta. latexx.sh muuntaa  svg kuvat eps kuviksi, mikäli inkscape on asennettuna.

--- latexx.sh
+++ latexx.sh
@@ -1,3 +1,11 @@
+# Kayta inkscapea muuntamaan svg kuvat eps kuviksi, jotka voidaan lisata suoraan
+# dokumenttiin
+for file in ./figures/*.svg
+do
+    inkscape -E ${file%.svg}.eps $file
+done
+
+
 pdflatex main.tex
 makeindex -s main.ist -t main.glg -o main.gls main.glo
 biber main
